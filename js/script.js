25804c688b69e4fb3e42a88fc9f97365e6faa10c
Xiaofei Zhang <abigcleverdog@gmail.com>
2017-07-06 23:50:31 -0400

    trimmed before 'beautification'

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 var locations = [
-	  {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}, id: 0},
-	  {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}, id: 1},
-	  {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}, id: 2},
-	  {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}, id: 3},
-	  {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}, id: 4},
-	  {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}, id: 5}
+	  {title: 'Harry Buffalo', location: {lat: 28.541346, lng: -81.381661}, id: 0},
+	  {title: 'Omega Deli Restaurant', location: {lat: 28.546058, lng: -81.373764}, id: 1},
+	  {title: 'Shari Sushi Lounge', location: {lat: 28.542288, lng: -81.369215}, id: 2},
+	  {title: 'LE GOURMET BREAK', location: {lat: 28.540667, lng: -81.377412}, id: 3},
+	  {title: "Panino's Pizza & Grill", location: {lat: 28.541595, lng: -81.378847}, id: 4},
+	  {title: "Leo's Diner", location: {lat: 28.546354, lng: -81.378952}, id: 5}
 	];
 
 var map;
 var myOption = {
-	  center: {lat: 40.7413549, lng: -73.9980244},
-	  zoom: 13,
+	  center: {lat: 28.548336, lng: -81.379234},
+	  zoom: 15,
 	  mapTypeControl: false
 	};
 var markers = [];
 var largeInfowindow;
 
+// Start Google Map error handling.
 var googleRequestTimeout = setTimeout(function(){
 		var el = document.createElement('p');
 		el.innerHTML = 'Failed to get Google Map resources...';
@@ -23,13 +24,15 @@ var googleRequestTimeout = setTimeout(function(){
 	}, 5000);
 
 function initMap() {
-console.log('A');
+
 	// Constructor creates a new map - only center and zoom are required.
 	map = new google.maps.Map(document.getElementById('map'), myOption);
-console.log('B');
+
 	// Initiate infoWindow.
-	largeInfowindow = new google.maps.InfoWindow();
-console.log('C');
+	largeInfowindow = new google.maps.InfoWindow({
+		maxWidth: 200
+	});
+
 	// Create an array of markers based on the locations data.
 	for (var i = 0; i < locations.length; i++) {
 	    var position = locations[i].location;
@@ -50,7 +53,7 @@ console.log('C');
 		});
 		
 	}
-console.log('D');
+
 // Build the VM. Seems the VM has to be nested in the initMap func to use google js.
 function LocationsViewModel() {
 
@@ -93,17 +96,18 @@ btn.addEventListener('click', function(){
 		populateInfoWindow(marker, largeInfowindow);
 	};
 }
-console.log('E');
+
 ko.applyBindings(new LocationsViewModel());
-console.log('F');
+
+// Stop error handling.
 clearTimeout(googleRequestTimeout);
 
 }
 
 
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
+// This function populates the infowindow when the marker is clicked. 
+// We'll only allow one infowindow which will open at the marker 
+// that is clicked, and populate based on that markers position.
 function populateInfoWindow(marker, infowindow) {
 	var infoContent;
 	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
@@ -111,10 +115,30 @@ function populateInfoWindow(marker, infowindow) {
 		'api-key': "3b237762e7c04b87889c9de7e953f24c",
 		'q': marker.title
 		});
-console.log('I am here');
+
+	// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+	    if (infowindow.marker) { infowindow.marker.setAnimation(null); }
+	    // You are selected! Let's BOUNCE!
+	    marker.setAnimation(google.maps.Animation.BOUNCE);
+	    infowindow.marker = marker;
+		var temp = '<div class="infowindow"><h4 id="nytimes-header"></h4>'
+		temp += '<ul id="nytimes-articles" class="article-list"></ul></div>'
+	    infowindow.setContent(temp);
+	    infowindow.open(map, marker);
+	    // Clear marker when the infowindow is closed.
+	    infowindow.addListener('closeclick', function() {
+			infowindow.marker.setAnimation(null);
+			infowindow.marker = null;
+	    });
+	} else {
+		toggleBounce(marker);
+	}
+		
+	// Fetch NYT articles to put in infoWindow & error handling.
 	$.getJSON( url, function( data ) {
-console.log('getJSON');
-		$('#nytimes-header').text('New York Times Articles About ' + marker.title);
+		$('#nytimes-header').text(
+			'New York Times Articles About ' + marker.title);
 		$.each( data['response']['docs'], function( key, val ) {
 		headline = val['headline']['main'];
 		url = val['web_url'];
@@ -126,28 +150,14 @@ console.log('getJSON');
 		});
 			
 	}).error(function() {
-		$('#nytimes-header').text('New York Times Articles Could Not Be Loaded');
+		$('#nytimes-header').text(
+			'New York Times Articles Could Not Be Loaded');
 });
-console.log(infoContent);
-	// Check to make sure the infowindow is not already opened on this marker.
-	if (infowindow.marker != marker) {
-	    if (infowindow.marker) { infowindow.marker.setAnimation(null); }
-	    /* markers[infowindow.marker.id].setAnimation(null); */
-	    marker.setAnimation(google.maps.Animation.BOUNCE);
-	    infowindow.marker = marker;
-	    infowindow.setContent('<div><h4 id="nytimes-header"></h4><ul id="nytimes-articles" class="article-list"></ul></div>');
-	    infowindow.open(map, marker);
-	    // Make sure the marker property is cleared if the infowindow is closed.
-	    infowindow.addListener('closeclick', function() {
-			infowindow.marker = null;
-	    });
-	} else {
-		toggleBounce(marker);
-	}
+
 }
 
 function toggleBounce(marker) {
-	console.log('I am called');
+	// BOUNCE or not? it is a question.
 	if (marker.getAnimation() !== null) {
 	  marker.setAnimation(null);
 	} else {
@@ -156,12 +166,8 @@ function toggleBounce(marker) {
 }
 
 
-function menuIcon(x) {
-    x.classList.toggle("change");
-    $('.options-box').toggle("change");
-}
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// Toggle between adding and removing the "responsive" class to 
+// the side list when the user clicks on the icon
 function myFunction() {
     var x = document.getElementById("options-box");
     if (x.className === "options-box") {
@@ -170,40 +176,6 @@ function myFunction() {
         x.className = "options-box";
     }
 }
-
-
-//////////function bone yard.
-
-// This function will loop through the markers array and display them all.
-function showListings(arr) {
-console.log('show func');	
-console.log(arr);	
-	var bounds = new google.maps.LatLngBounds();
-	console.log('show');
-	console.log(markers[0]);
-	// Extend the boundaries of the map for each marker and display the marker
-	for (var i = 0; i < markers.length; i++) {
-	    markers[i].setMap(map);
-		bounds.extend(markers[i].position);
-	}
-	map.fitBounds(bounds);
-}
-
-  // This function will loop through the listings and hide them all.
-  function updateListings(arr) {
-	console.log('update here');
-	console.log(arr[0].id);
-	console.log(arr.length);
-	console.log(markers[0]);
-	console.log(markers[arr[0].id]);
-	for (var i = 0; i < markers.length; i++) {
-	  markers[i].setMap(null);
-	}
-	/* for (var i = 0; i < arr.length; i++) {
-	  markers[i].setMap(map);
-	} */
-  }
-
 
 
 
